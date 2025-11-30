9cf2ee1906bb44d241e584a76bd9adfbbf3eb066
Arjun_2025 <vineetaarjun123@gmail.com>
2025-11-30 23:45:42 +0530

    imp bg file

--- chat.cpp
+++ chat.cpp
@@ -3,7 +3,7 @@ execution:
 g++ chat.cpp -o chat
 ./chat
 */
-
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,4 +12,30 @@ int main(){
     cout<<"hola"<<endl;
     return 0;
 
+    
 }
+    */
+
+    // data race
+#include<bits/stdc++.h>
+#include <iostream>
+#include <thread>
+
+int counter = 0; // shared variable
+
+void increment() {
+    for (int i = 0; i < 100000; ++i) {
+        counter++;  // ⚠ data race here
+    }
+}
+
+int main() {
+    std::thread t1(increment);
+    std::thread t2(increment);
+
+    t1.join();
+    t2.join();
+
+    std::cout << "Counter: " << counter << std::endl;
+}
+
--- chat.py
+++ chat.py
@@ -1,3 +1,60 @@
 ## py🐍🐍
-print("hello from arjun")
+#print("hello from arjun")
 
+size = 100
+stack  = [0] * size
+top = -1
+
+def push(value):
+    global top
+    if top == size - 1:
+        print("Stack is full.")
+        return 
+    top += 1
+    stack[top] = value
+    print(f"BOOK ID {value} pushed in the shelf.")
+
+def pop():
+    global top
+    if top == -1:
+        print("Stack underflow.")
+        return
+    element = stack[top]
+    top -= 1
+    print(f"bookid {element} removed from the shelf.")
+
+def displayStack():
+    if top == -1:
+        print("Empty shelf.")
+        return
+    
+    for item in reversed(stack):
+        print(item,end=" ")
+    print()
+
+
+
+def main():
+    while True:
+        try:
+            nums = input().strip().split()
+            if not nums:
+                continue
+            choice = int(input[0]);
+            if choice == 1:
+                value = int(input[1])
+                push(value)
+            elif choice == 2:
+                pop()
+            elif choice == 3:
+                displayStack()
+            elif choice == 4:
+                print("exiting the program.")
+                break
+            else:
+                print("invalid choice")
+        except:
+            continue
+          
+    
+main()
